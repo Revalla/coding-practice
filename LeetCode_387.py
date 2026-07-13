d92e36e8f33733c6052900af474715110939ec8a
Revalla Harika <148746271+Revalla@users.noreply.github.com>
2026-07-13 21:43:42 +0530

    Create LeetCode_387.py

---
+++ LeetCode_387.py
@@ -0,0 +1,12 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        d={}
+        for i in s:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        for j in range(len(s)):
+            if d[s[j]]==1:
+                return j
+        return -1
