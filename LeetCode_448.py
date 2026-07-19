0f94dd934c3156f59b30ccf07238eb7ef137f4c2
Revalla Harika <148746271+Revalla@users.noreply.github.com>
2026-07-19 21:37:44 +0530

    Implement function to find disappeared numbers

---
+++ LeetCode_448.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        result = []
+        s = set(nums)
+        for i in range(1, len(nums) + 1):
+            if i not in s:
+                result.append(i)
+        return result
